efe79c6ab65b26b826614b02098b04b8216a518c
Javad Bahman Abadi <jbasoft1@gmail.com>
2023-06-11 22:04:12 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,13 +10,5 @@ echo ""
 read -p "Please Enter Your Email :" emailAdr
 read -p "Please Enter Your sub domain :" subDomain
 echo -e "${yellow}Your email will be: ${emailAdr} and subdomain : ${subDomain}${plain}"
-#install 
-apt update && apt upgrade -y
-apt install curl socat -y
-~/.acme.sh/acme.sh --set-default-ca --server letsencrypt
-~/.acme.sh/acme.sh --register-account -m ${emailAdr}
-~/.acme.sh/acme.sh --issue -d ${subDomain} --standalone
-~/.acme.sh/acme.sh --installcert -d ${subDomain} --key-file /root/private.key --fullchain-file /root/cert.crt
-echo -e "${yellow} === Your Private Key Stored in => /root/private.key & cert file path => /root/cert.crt ${plain}"
-echo -e "${green}Now We Are going to install 3 x-ui (v2ray) ..."
-bash <(curl -Ls https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh)
+
+
