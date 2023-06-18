98c20a63d62fb7b70d08e8aa786543b58066c426
Javad Bahman Abadi <jbasoft1@gmail.com>
2023-06-18 22:11:23 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,8 +9,9 @@ echo "..::: welcome to install v2ray :::..."
 echo ""
 read -p "Please Enter Your Email :" emailAdr
 read -p "Please Enter Your sub domain :" subDomain
-echo -e "${yellow}Your email will be: ${emailAdr} and subdomain : ${subDomain}${plain}"
-#install 
+echo -e "${yellow} Your email will be: ${emailAdr}${plain}"
+echo -e " and subdomain : ${subDomain}${plain}"
+
 apt update && apt upgrade -y
 apt install curl socat -y
 curl https://get.acme.sh | sh
@@ -18,7 +19,13 @@ curl https://get.acme.sh | sh
 ~/.acme.sh/acme.sh --register-account -m ${emailAdr}
 ~/.acme.sh/acme.sh --issue -d ${subDomain} --standalone
 ~/.acme.sh/acme.sh --installcert -d ${subDomain} --key-file /root/private.key --fullchain-file /root/cert.crt
-echo -e "${yellow} === Your Private Key Stored in => /root/private.key & cert file path => /root/cert.crt ${plain}"
-echo -e "${green}Now We Are going to install 3 x-ui (v2ray) ..."
-bash <(curl -Ls https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh)
+echo -e "${yellow}Your Private Key Stored in => /root/private.key & cert file path => /root/cert.crt${plain}"
+echo -e "${green}Now We Are going to install 3 x-ui (v2ray) ...${plain}"
 
+read -p "Do you want to install 3x-ui or no (old version) [y/n]? ": config_confirm
+ if [[ "${config_confirm}" == "y" || "${config_confirm}" == "Y" ]]; then
+    bash <(curl -Ls https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh)
+else
+    bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh)
+fi
+echo -e "${green}Running...${plain}"
